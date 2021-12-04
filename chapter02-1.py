2594fca830c7dabd293ffedd634b2dda1b5edabb
knowbaesu <vkrwltn12@gmail.com>
2021-12-04 16:31:36 +0900

    second

--- chapter02-1.py
+++ chapter02-1.py
@@ -35,3 +35,35 @@ print()
 print('%s %s' % ('one', 'two'))
 print('{} {}'.format('one','two'))
 print('{1} {0}'.format('one', 'two'))
+
+# %s
+print('%10s' % ('nice'))
+print('{:>10}' .format('nice'))
+
+print('%-10s' % ('nice'))
+print('{:10}' .format('nice'))
+
+
+print('{:^10}' .format('nice'))
+
+print('%5s' % ('nice'))
+print('%.5s' % ('nicepyton')) #.은 절삭을 의미. 원래는  niceptyhon 다들어와야하는데 nicep 5글자만 들어온거 확인할 수 있디.
+print('{:10.5}' .format('pythonstudy'))
+
+# %d
+
+print('%d %d'  % (1,2) )
+print('{} {}' .format(1,2))
+
+print('%4d'% (423434))
+print('{:4d}'  .format(42))
+
+
+# %f
+
+print('%1.8f' % (3.141332434))
+print('{:f}'. format(3.14134343))
+
+
+print('%06.2f' % (3.1432234234))
+print('{:06.2f}' .format(3.1432234234))
