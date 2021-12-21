c756dd419f473076974b44d4cd0d3405cafaa6b5
Ji Soo <vkrwltn12@gmail.com>
2021-12-21 23:45:28 +0900

    05-02

--- chapter04-02.py
+++ chapter04-02.py
@@ -7,120 +7,86 @@
 #     <loop body>
 
 for v1 in range(10):
-    print("v1 is :", v2)
+    print("v1 is :", v1)
 
 print()
 
 for v2 in range(1, 11):
-    print("v2 is :", v3)
+    print("v2 is :", v2)
 
 print()
 
 for v3 in range(1, 11, 2):
-    print("v3 is :", v4)
+    print("v3 is :", v3)
 
 print()
 
-# 1 ~ 1000합
+# 1 부터 1000합
 
 sum1 = 0
 
 for v in range(1, 1001):
-    sum1 += v
+    sum1 += v  # sum1 = sum1 + v
+print('1 ~ 1000 Sum :' , sum1)
 
-print('1 ~ 1000 Sum : ', sum1)
+print('1 ~ 1000 Sum :' , sum(range(1, 1001)))
 
-print('1 ~ 1000 Sum : ', sum1)
-print('1 ~ 1000 Sum : ', sum(range(1, 1001)))  # sum(리스트)
-print('1 ~ 1000 안에 4의 배수의 합 : ', sum(range(1, 1001, 4)))
+print(type(range(1, 11)))
 
+print('1 ~ 1000 4의 배수의 합 :' , sum(range(4, 1001, 4)))
 
 # Iterables 자료형 반복
-# 문자열, 리스트, 튜플, 집합, 사전
+# 문자열, 리스트, 튜플 , 집합, 사전(딕셔너리)
 # iterable 리턴 함수 : range, reversed, enumerate, filter, map, zip
 
-# 예제1
-names = ["Kim", "Park", "Cho", "Lee", "Choi", "Yoo"]
+# 예제 1
+names = ['Kim', 'Park', 'Cho', 'Lee', 'Choi', 'Yoo']
 
-for name in names:
-    print("You are", name)
+for n in names:
+    print('You are :' , n)
 
 # 예제2
 lotto_numbers = [11, 19, 21, 28, 36, 37]
 
-for number in lotto_numbers:
-    print("Current number : ", number)
+for n in lotto_numbers:
+    print("Current number : ", n )
 
-# 예제3
-word = 'Beautiful'
+# 예제 3
 
-for s in word:
+word = "Beautiful"
+
+for s in word :
     print('word : ', s)
 
 # 예제4
 my_info = {
-    "name": "Lee",
-    "Age": 33,
+    "name": 'Lee',
+    "Age" :33,
     "City": "Seoul"
 }
 
 for key in my_info:
-    print(key, ":", my_info[key])
+    print('key:', my_info[key])
 
-for val in my_info.values():
-    print(val)
+for v in my_info.values():
+    print('value:', v )
 
 # 예제5
-name = 'FineApplE'
 
-for n in name:
-    if n.isupper():
-        print(n)
+name = 'FimeAppLE'
+for i in name:
+    if i.isupper() :
+        print(i)
     else:
-        print(n.upper())
-
-numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+        print(i.upper())
 
 # break
-for num in numbers:
-    if num == 34:
-        print("Found : 34!")
+
+number = [14, 3, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
+
+for n in number:
+    if n == 34:
+        print('Found: 34!')
         break
     else:
-        print("Not found : ", num)
-
-# continue
-lt = ["1", 2, 5, True, 4.3, complex(4)]
-
-for v in lt:
-    if type(v) is bool:
-        continue
-
-    print("current type : ", type(v))
-    print("multiply by 2:", v * 3)
-
-# else 구문
-numbers = [14, 3, 4, 7, 10, 24, 17, 2, 33, 15, 34, 36, 38]
-
-for num in numbers:
-    if num == 34:  # 45
-        print("Found : 34!")
-        break
-else:
-    print("Not Found 45...")
-
-
-# 구구단 출력
-
-for i in range(2, 10):
-    for j in range(2, 10):
-        print('{:4d}'.format(i * j), end='')
-    print()
-
-
-# 변환 예제
-name = 'Aceman'
-print('Reversed : ', reversed(name))
-print('List : ', list(reversed(name)))
-print('Tuple : ', tuple(reversed(name)))
-print('Set : ', set(reversed(name)))  # 순서X
+        print('Not found :', n) 
